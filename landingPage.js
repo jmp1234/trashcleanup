3b2655ab7f0c2164717109ec81c8c8e2d4ad0670
jmp1234 <johnnypham944@yahoo.com>
2019-03-20 13:06:25 -0700

    fixed the error on map load

--- landingPage.js
+++ landingPage.js
@@ -22,6 +22,14 @@ class LandingPage{
         const locations = eventLocations;
 
 
+        mapboxgl.accessToken = 'pk.eyJ1IjoiamVuLWwiLCJhIjoiY2p0ZmR2bm8zMDJ4bDN5cGp2ZDk1cmhweCJ9.P0S6-ZdkFBaOaw0V0Q868A';
+          this.map = new mapboxgl.Map({
+                container: 'map',
+                style: 'mapbox://styles/mapbox/streets-v9',
+                center: [-117.956556, 33.630271],
+                zoom: 10,
+                minZoom: 10
+          });
 
         // add markers to map
         locations.forEach(marker => {
