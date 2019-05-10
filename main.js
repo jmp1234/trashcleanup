6dec39e788e24cc51baa117c9cf6d5ffe1259ade
Jennifer Lai <jenniferlai.81@gmail.com>
2019-05-09 21:35:41 -0700

    fixed facebook share button from initializing app twice, clean up code in main.js and landingPage.js

--- main.js
+++ main.js
@@ -3,12 +3,5 @@ $(document).ready(initializeApp);
 var landingPage = null;
 
 function initializeApp() {
-    console.log('hi')
-    $('#introModal').modal({
-        fadeDuration: 100,
-        show: true,
-    });
     landingPage = new LandingPage();
-    landingPage.addClassToSidebarButton();
-    landingPage.toggleSidebar();
 }
